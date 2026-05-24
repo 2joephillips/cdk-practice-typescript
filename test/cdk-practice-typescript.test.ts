a33685d961422d26bd877390f53ffcfea00c77dd
Joseph Phillips <joseph@josephphillips.co>
2026-05-24 15:45:21 -0400

    Initial commit

---
+++ test/cdk-practice-typescript.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkPracticeTypescript from '../lib/cdk-practice-typescript-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-practice-typescript-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkPracticeTypescript.CdkPracticeTypescriptStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
